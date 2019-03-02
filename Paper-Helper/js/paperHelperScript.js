66a73ed15060601ac45a2a60eada8c73d11e89fb
Lukas Michaels <lmichaels@apple.com>
2019-03-01 17:28:17 -0800

    New commit

--- Paper-Helper/js/paperHelperScript.js
+++ Paper-Helper/js/paperHelperScript.js
@@ -1,20 +1,13 @@
-function saveQuestion1(){
-	var q1 = document.getElementById("field1").value;
-	localStorage.setItem('question1', q1);
-	var question = localStorage.getItem(question1);
-}
 function questionUpdate(){
 	var question = localStorage.getItem('question1');
 	document.getElementById("question").innerHTML = "<h5><i>" + question + "</i></h5>";
 }
 
-function test(){
-	var question = localStorage.getItem('question1');
-	//document.getElementById("question").innerHTML = question;
-	console.log(question);
+function saveQuestion1(){
+	var q1 = document.getElementById("field1").value;
+	localStorage.setItem('question1', q1);
+	var question = localStorage.getItem(question1);
 }
-
-
 
 function saveQuestion2(){
 	var q2 = document.getElementById("field2").value;
